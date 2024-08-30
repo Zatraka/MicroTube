d98871da569cc36b39e0eaec1eafb068be541d36
Zatraka <mats.bechtold@t-online.de>
2024-08-30 08:13:52 +0200

    -

--- Frontend/Version_01/src/App.tsx
+++ Frontend/Version_01/src/App.tsx
@@ -1,11 +1,42 @@
+import logo from 'C:/Users/User/Desktop/Git-R/MicroTube/Frontend/Version_01/src/assets/Icons/logo.png';
+import React, { useState } from 'react';
 
-import React from "react";
+function ButtonHomepatch({ title }: { title: string }) 
+{
+  const [color, setColor] = useState('red');
 
-const App = () => {
+  // Funktion, um die Farbe zu ändern, wenn der Button geklickt wird
+  const handleClick = () => 
+  {
+    setColor(color === 'red' ? 'green' : 'red');
+  };
+
+  return (
+    <button 
+      onClick={handleClick}
+      style={{ backgroundColor: color }} 
+    >
+      {title}
+    </button>
+  );
+}
+
+
+export function Profile({ name }:{ name:string }) 
+{
+  return (
+      <img src={logo} alt="mein YT-Logo" width={600} height={400} />
+  )
+}
+
+const App = () => 
+  {
   return (
     <div>
-
-
+      <h1>Test Button</h1>
+      <ButtonHomepatch title="Test01 Button" />
+       <br></br>
+       <Profile name = "h"/>
     </div>
   )
 
