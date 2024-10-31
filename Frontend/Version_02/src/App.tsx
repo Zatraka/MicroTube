14c7e1400249dd1984a7d0355299060f50ac5f88
Zatraka <mats.bechtold@t-online.de>
2024-10-31 10:50:02 +0100

    -

--- Frontend/Version_02/src/App.tsx
+++ Frontend/Version_02/src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 //import Router from 'react-router-dom'
-
+//import { Link } from 'react-router-dom' // wird für Links benutzt.
+  
+/* <Link to={"./Dashboard"}>
+  Dashboard
+</Link> */
 
 const App = () => {
   return (
     <div>
       <Navbar />
-    
+      {/* <Link></Link> // Home-Page*/} 
+      {/* <Link></Link> // Login-Page*/} 
+      {/* <Link></Link> // Kanalvorlage-Page*/} 
+      {/* <Link></Link> // Video-Page*/} 
+
     </div>
   )
 }
