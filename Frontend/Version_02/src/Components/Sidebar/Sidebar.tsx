9f8dbae09c2f80bbb33b23ea87fc6524f7a91b7d
Zatraka <mats.bechtold@t-online.de>
2024-12-20 16:33:21 +0100

    Der Sidebar Css hinzugefügt.

--- Frontend/Version_02/src/Components/Sidebar/Sidebar.tsx
+++ Frontend/Version_02/src/Components/Sidebar/Sidebar.tsx
@@ -1,35 +1,52 @@
 
 //import React from 'react'
+import './Sidebar.css'
 import home from '../../assets/Icons/home.png'
 import aboList from '../../assets/Icons/folders.png'
 import myYoutube from '../../assets/Icons/user-gear.png'
 import history from '../../assets/Icons/time-past.png'
-
+import profile_Icon from '../../assets/Icons/OneUser.png'
 
 
 const Sidebar = () => {
     return(
         <div className="Sidebar">
-            <div className="sortcut-links"> {/**/}
+            <div className="shortcut-links">
                 <div className="side-link">
-                    <img src={home} alt ="icon 1"/><p>Startseite</p>
+                    <img src={home} alt ="shortcut-icon 1"/><p>Startseite</p>
                 </div>
                 <div className="side-link">
-                    <img src={aboList} alt =" "/><p>Abos</p>
+                    <img src={aboList} alt ="shortcut-icon 2"/><p>Abos</p>
                 </div>
                 <div className="side-link">
-                <img src={myYoutube} alt =" "/><p>Mein Youtube</p>
+                <img src={myYoutube} alt ="shortcut-icon 3"/><p>Mein Youtube</p>
                 </div>
                 <div className="side-link">
-                    <img src={history} alt =" "/><p>Verlauf</p>
+                    <img src={history} alt ="shortcut-icon 4"/><p>Verlauf</p>
                 </div>
                 <div className="side-link">
-                    <img src={home} alt =" "/><p>Home</p>
+                    <img src={home} alt ="shortcut-icon 5"/><p>Home</p>
                 </div>
                 <div className="side-link">
-                <img src={home} alt =" "/><p>Home</p>
+                <img src={home} alt ="shortcut-icon 6"/><p>Home</p>
                 </div>
+                <hr />
+            </div>
 
+            <div className='subscribedList'>
+                <h3> Subscribed</h3>
+                <div className="side-link">
+                    <img src={profile_Icon} alt="" /> <p>Acountname</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile_Icon} alt="" /> <p>Acountname</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile_Icon} alt="" /> <p>Acountname</p>
+                </div>
+                <div className="side-link">
+                    <img src={profile_Icon} alt="" /> <p>Acountname</p>
+                </div>
             </div>
         </div>
     );
