ba95bd5eb5bf3aaf4378a9dfd5282661510ea044
Zatraka <mats.bechtold@t-online.de>
2024-12-30 19:42:29 +0100

    -

--- Frontend/Version_02/src/Components/Recommendation/Recommendation.tsx
+++ Frontend/Version_02/src/Components/Recommendation/Recommendation.tsx
@@ -1,11 +1,57 @@
 import React from 'react'
-import './recommendation.css'
+import './Recommendation.css'
+
+//thumbnails
+import thumbnail01 from '../../assets/Images/stockmarket.jpg'
+import thumbnail02 from '../../assets/Images/sad_dog_face.jpg'
+import thumbnail03 from '../../assets/Images/lake_trees.jpg'
+import thumbnail04 from '../../assets/Images/ape_family.jpg'
+import thumbnail05 from '../../assets/Images/race_car.jpg'
 
 
 const Recommendation = () => {
   return (
-    <div>
-
+    <div className='recommendation'>
+        <div className="side-video-list">
+            <img src={thumbnail01} alt="" />
+            <div className="vid-info">
+                <h4>Videotitel</h4>
+                <p>Kanalname</p>
+                <p>100k Total-Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail02} alt="" />
+            <div className="vid-info">
+                <h4>Videotitel</h4>
+                <p>Kanalname</p>
+                <p>100k Total-Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail03} alt="" />
+            <div className="vid-info">
+                <h4>Videotitel</h4>
+                <p>Kanalname</p>
+                <p>100k Total-Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail04} alt="" />
+            <div className="vid-info">
+                <h4>Videotitel</h4>
+                <p>Kanalname</p>
+                <p>100k Total-Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail05} alt="" />
+            <div className="vid-info">
+                <h4>Videotitel</h4>
+                <p>Kanalname</p>
+                <p>100k Total-Views</p>
+            </div>
+        </div>
     </div>
   )
 }
--- Frontend/Version_02/src/Pages/Video/Video.tsx
+++ Frontend/Version_02/src/Pages/Video/Video.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Video.css'
 import PlayVideo from '../../Components/Play/playVideo'
-import Recommendation from '../../Components/Recommendation/Recommendation'
+import Recommendation from '../../Components/Recommendation/recommendation'
 
 const Video = () => {
   return (
     <div className='play-container'>
       <PlayVideo/>
-      <Recommendation></Recommendation>
+      <Recommendation/>
     </div>
   )
 }
